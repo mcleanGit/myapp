b2fe2c4298ebcee8407f43b0ae29a3b91f7d1187
Don McLean <mclean@utoronto.ca>
2022-05-27 12:09:46 -0400

    add competencies paragraphs

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,6 +5,13 @@ function Resume() {
     <section className="my-5">
       <h1 className="resume">
       <a href="https://music.utoronto.ca/our-people.php?fid=4">Resume</a> </h1>
+
+      <p>Experience and Proficiencies</p>
+      <p>Higher Education and Arts Administration</p>
+      <p>Research in Music Theory and Musicology</p>
+      <p>Project Management: digital media innovation and major infrastructure</p>
+      <p>Coding: basic competencies: HTML, CSS, Git, CSS, JS, APIs, Node.js</p>
+      <p>OOP, Express.js, SQL, ORM, MVC, Regex, NoSQL, PWA, React, MERN, State</p>
     </section>
   );
 }
